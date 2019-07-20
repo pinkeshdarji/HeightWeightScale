01012d218d0cbe2a5fae491adc52610cde80c033
pinkesh <pinkesh.earth@gmail.com>
2019-07-20 12:24:27 +0530

    First push.

---
+++ android/app/src/main/kotlin/com/auberginesolutions/height_weight_scale/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.auberginesolutions.height_weight_scale
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
